d598ece43e12a9860ead9c99a98687a2e577e4bd
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-05-09 13:34:47 +0200

    Updated exemple

--- acf-layouts/slider/Slider.php
+++ acf-layouts/slider/Slider.php
@@ -3,10 +3,4 @@ class Slider extends ACFModule {
     public function template() {
         return 'slider-template.php';
     }
-    public function setFontColor($fontColor) {
-        $this->font_color = $fontColor ;
-    }
-    public function getFontColor(){
-        return $this->font_color;
-    }
 }
