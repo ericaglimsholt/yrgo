dfdde8d31e42facad55f88cbce6d615d101fe905
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-05-09 13:09:15 +0200

    Added all features

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Our main CSS -->
+        <link rel="stylesheet" href="<?php wp_enqueue_style( 'style', get_stylesheet_uri() ); ?>">
+        <?php wp_head(); ?>
+
+        <!-- Bootstrap CSS -->
+       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+        <title>Yrgo</title>
+
+    </head>
+    <body>
+
+        <!-- Start of container div, ends in footer.php -->
+        <div class="container">
