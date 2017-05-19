59b8c6de9d927f948edb131a37bb25a4fe611497
Joakim <joakim@idrottskoll.se>
2017-05-19 10:54:34 +0200

    testing new branch

--- acf-layouts/s-news/news-template.php
+++ acf-layouts/s-news/news-template.php
@@ -1,6 +1,11 @@
 <!-- close container -->
 </div>
 
+<?php $educations = get_posts([
+  'post_type' => 'news',
+  'order' => 'ASC',
+  'orderby' => 'name'
+]); ?>
 
 
 
@@ -11,7 +16,17 @@
     <div class="s-header-background col-md-12" style="background-image:url(<?= $this->getBackgroundImg() ?>);">
 
       <div class="container s-news">
-        <p>NYHETER</p>
+        <p class=" s-news-header">NYHETER</p>
+
+          <?php foreach ($educations as $education): ?>
+            <div class="s-news-list">
+              <p class="s-news-list-header"><?php echo $education->post_title ?></p>
+              <p>----</p>
+              <a href="<?php echo $education->guid ?>"><p>Läs mer</p></a>
+            </div>
+
+
+          <?php endforeach; ?>
 
       </div>
 
