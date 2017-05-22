862105ab5d7c8a2a7b3bedecffb81c33c0dadbb7
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-05-22 14:39:25 +0200

    Something

--- acf-layouts/a-activitiesContent/activitiesContent.php
+++ acf-layouts/a-activitiesContent/activitiesContent.php
@@ -1,39 +1,14 @@
-<!-- close container -->
-</div>
-<?php $activities = get_posts([
-  'post_type' => 'Activities',
-  'order' => 'ASC',
-  'orderby' => 'name'
-]); ?>
+<?php
+class NewsContent extends ACFModule {
+    public function template() {
+        return 'activitiesContent-template.php';
+    }
+    public function setActivitiesContentField($activitiesContent) {
+      $this->activitiesContent = $activitiesContent;
+    }
 
-<!-- Get and escape id from url -->
-<?php $id = esc_html( $_REQUEST['ID'] ) ?>
+    public function getActivitiesContentField(){
+      return $this->activitiesContent;
+    }
 
-  <?php foreach ($activities as $activitie): ?>
-    <?php if ($activitie->ID == $id && have_rows('n-news', $id)): ?>
-      <?php while (have_rows('n-news', $id)): ?>
-        <?php the_row(); ?>
-
-            <div class="container">
-              <div class="row s-newsContent">
-
-                <div class="col-12">
-                  <p class="headline"><?php print get_sub_field('headline')?></p>
-                </div>
-
-                <div class="col-6">
-                  <p><?php print get_sub_field('content')?></p>
-                </div>
-
-                <div class="col-6">
-                  <p><?php print get_sub_field('content')?></p>
-                </div>
-
-              </div>
-            </div>
-
-<?php endwhile; endif; endforeach; ?>
-
-
-
-<div class="container">
+}
