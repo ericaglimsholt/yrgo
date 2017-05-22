c50b076f6621cbe809fa698f65300aa0724bfb17
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-05-22 14:51:44 +0200

    rm

--- acf-layouts/a-activitiesContent/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('ActivitiesContent.php');
-
-$activitiesContent = new ActivitiesContent;
-$activitiesContent->activitiesContent = get_sub_field('activitiesContent');
-$activitiesContent->printHTMl();
