7a7798a0ea9febe6cca7c3e3a35a04b1c936a010
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-05-23 11:20:32 +0200

    Added links

--- acf-layouts/y-links/links-template.php
+++ acf-layouts/y-links/links-template.php
@@ -1,13 +1,32 @@
+<?php
+
+$educations = get_posts (array(
+    'name' => 'Test Taxonomy Multicheck',
+    'id' => $prefix . 'choose_education',
+    'taxonomy' => 'category', //Enter Taxonomy Slug
+    'type' => 'taxonomy_multicheck',
+));
+
+ ?>
+
 <!-- Closes container div -->
 </div>
 
-<div class="row">
+<div class="row links-nav">
 
     <?php if( have_rows('educations_links') ): ?>
       <?php while( have_rows('educations_links') ): the_row();   ?>
         <div class="col-md-6 links">
         <!-- Outputs the startpage image -->
         <img class="education-img" src="<?= get_sub_field('image') ?>" alt="">
+        <div class="container">
+
+            <?php foreach ($educations as $education): ?>
+              <?php var_dump($education) ?>
+                <li class="education"><?php echo $education->post_title ?></li>
+            <?php endforeach; ?>
+
+        </div>
         </div>
     <?php endwhile; endif?>
 
