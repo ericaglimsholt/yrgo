3be7f7628f9c10abffee5609a14c8c45ee183cd9
Joakim <joakim@idrottskoll.se>
2017-05-24 00:03:11 +0200

    added hover color

--- acf-layouts/s-links/links-template.php
+++ acf-layouts/s-links/links-template.php
@@ -8,9 +8,11 @@
       <?php while( have_rows('links') ): the_row();   ?>
 
         <div class="col-6 s-links-CenterContent" data-color="<?= get_sub_field('opacity-color') ?>" style="background-image:url(<?= get_sub_field('background') ?>);">
-          <h2><?= get_sub_field('headline') ?></h2>
-          <a href="<?= get_sub_field('link') ?>"><p>Läs mer</p></a>
+          <a href="<?= get_sub_field('link') ?>">
+            <h2><?= get_sub_field('headline') ?></h2>
+          </a>
         </div>
+
     <?php endwhile; endif?>
 
   </div>
@@ -18,8 +20,19 @@
 
 <div class="container">
 
-<!-- <script>
-  alert("test")
-  elementList = document.querySelectorAll(selectors);
+<script>
+  links = document.querySelectorAll('.s-links-CenterContent');
+  for (i = 0; i < links.length; i++) {
 
-</script> -->
+    links[i].addEventListener("mouseleave", function (e) {
+        this.style.backgroundColor='';
+        // console.log(this.dataset.color);
+    });
+
+      links[i].addEventListener("mouseenter", function (e) {
+          this.style.backgroundColor=this.dataset.color;
+          // console.log(this.dataset.color);
+      });
+  }
+
+</script>
