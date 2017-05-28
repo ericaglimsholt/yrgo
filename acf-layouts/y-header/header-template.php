2d4fc6f444ca3f76a56b8d41c4ad8640a5b5e769
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-05-28 19:59:43 +0200

    Added the ability to add educations

--- acf-layouts/y-header/header-template.php
+++ acf-layouts/y-header/header-template.php
@@ -18,19 +18,26 @@ $categories = get_categories( array(
       <div class="col-md-6 col-header">
         <h2><?= $this->getTitle() ?></h2>
         <h2 class="start"><?= $this->getStartTitle() ?></h2>
-
         <ul>
           <?php foreach ($categories as $category): ?>
-            <?php
-            // $term_link = get_term_link( $category ); ?>
-             <!-- <a href="<?php echo esc_url( $term_link ) ?>"> -->
                <li class="category"><?php  echo $category->name ?></li>
-
-              <!-- </a> -->
           <?php endforeach; ?>
-
         </ul>
+      </div>
 
+      <div class="col-md-6 col-header">
+        <?php
+        $posts = get_sub_field('education_single');
+        if( $posts ): ?>
+        <ul>
+          <?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
+            <a href="<?php echo $post->guid; ?>">
+            <li><?php  echo $post->post_title; ?></li>
+          </a>
+          <?php endforeach; ?>
+        </ul>
+        <?php wp_reset_postdata(); // IMPORTANT – reset the $post object so the rest of the page works correctly ?>
+        <?php endif; ?>
       </div>
     </div>
   </div>
