40f89744702cf4f9ca75e1ac8d8981954a0042f9
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-05-29 09:01:49 +0200

    Cleaned up

--- acf-layouts/y-links/links-template.php
+++ acf-layouts/y-links/links-template.php
@@ -1,43 +1,24 @@
-<?php
-$categories = get_categories( array(
-    'orderby' => 'name',
-    'order'   => 'ASC',
-));
-
-
-
-$educations = get_posts (array(
-    'name' => 'Test Taxonomy Multicheck',
-    'id' => $prefix . 'choose_education',
-    'taxonomy' => 'category', //Enter Taxonomy Slug
-    'type' => 'taxonomy_multicheck',
-));
-
- ?>
-
 <!-- Closes container div -->
 </div>
 
 <div class="row links-nav">
 
-
-
     <?php if( have_rows('educations_links') ): ?>
       <?php while( have_rows('educations_links') ): the_row();   ?>
 
-        <div class="col-md-6 links s-links-CenterContent removeImg">
-
-
-
+        <div class="col-md-6 links ">
         <!-- Outputs the startpage image -->
           <a href="<?= get_sub_field('link_education') ?>">
+            <!-- Outputs the opacity color -->
             <div class="container links-container s-links-CenterColor" data-color="<?= get_sub_field('opacity-color') ?>">
               <?php $term = get_sub_field('choose_education'); ?>
               <?php foreach ($term as $terma): ?>
+                <!-- Outputs the name from taxanomy -->
                 <h3><?php print $terma->name; ?></h3>
               <?php endforeach; ?>
             </div>
 
+            <!-- Outputs the education image -->
             <img class="education-img" src="<?= get_sub_field('image') ?>" alt="">
           </a>
         </div>
@@ -46,19 +27,18 @@ $educations = get_posts (array(
 
 </div>
 
+<!-- Script for show right opacity color on hover, choosen in dashboard -->
 <script>
   links = document.querySelectorAll('.s-links-CenterColor');
   for (i = 0; i < links.length; i++) {
 
     links[i].addEventListener("mouseleave", function (e) {
-        this.style.background='';
-        // console.log(this.dataset.color);
+      this.style.background='';
     });
 
-      links[i].addEventListener("mouseenter", function (e) {
-          // this.style.transition = "2s ease-in-out";
-          this.style.background=  'linear-gradient( to top, rgba(255,255,255,0), ' + this.dataset.color + ')';
-      });
+    links[i].addEventListener("mouseenter", function (e) {
+      this.style.background=  'linear-gradient( to top, rgba(255,255,255,0), ' + this.dataset.color + ')';
+    });
   }
 
 </script>
